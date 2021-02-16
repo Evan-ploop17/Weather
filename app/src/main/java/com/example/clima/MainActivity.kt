3485b79fa6557c1c0f2c047dca1d43cbdbcbae58
Esteban Salas <esalas21@uan.edu.co>
2021-02-16 13:31:34 -0500

    Agregamos el método para la solicitud HTTP con Volley

--- app/src/main/java/com/example/clima/MainActivity.kt
+++ app/src/main/java/com/example/clima/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.clima
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,24 +19,37 @@ class MainActivity : AppCompatActivity() {
 
         val city = intent.getStringExtra("city")
 
-        val bogota = City("Bogota", 12, "Cloudy")
-        val medellin = City("Medellin", 24, "Sunny")
+        if(Network.thereIsNetwork(this)){
+            // Execute HTTP request
+                Toast.makeText(this, "Hay red", Toast.LENGTH_LONG).show()
+                requestHTTPVolley("https://api.openweathermap.org/data/2.5/weather?id=3674962&appid=d83db4f259a5b6e14bb72ca03f55742d")
+        }else{
+            // Show a message
 
-        when (city) {
-            "Bogota" -> {
-                textDegrees.text = bogota.degrees.toString() + "º"
-                textCity.text = bogota.name
-                textState.text = bogota.state
-            }
-            "Medellin" -> {
-                textDegrees.text = medellin.degrees.toString() + "º"
-                textCity.text = medellin.name
-                textState.text = medellin.state
-            }
-            else -> {
-                Toast.makeText(this, "Information not found", Toast.LENGTH_LONG).show()
-            }
         }
 
+
     }
+
+    private fun requestHTTPVolley(url:String){
+
+        val queue = Volley.newRequestQueue(this)
+        val request = StringRequest(Request.Method.GET, url, Response.Listener<String>{
+            response->
+            try{
+                Log.d("http",response)
+
+                val gson = Gson()
+                val city = gson.fromJson(response, City::class.java)
+                Log.d("city", city.name)
+
+                textDegrees.text = city.main?.temp.toString() + "º"
+                textCity.text = city.name
+                textState.text = city.weather?.get(0)?.description
+            }catch (e: Exception){
+            }
+        }, Response.ErrorListener {})
+        queue.add(request)
+    }
+
 }
