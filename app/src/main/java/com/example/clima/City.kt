9bff675060f4af5dc337aa05be283991e089f903
Esteban Salas <esalas21@uan.edu.co>
2021-02-16 13:31:51 -0500

    Agregamos el método para la solicitud HTTP con Volley

--- app/src/main/java/com/example/clima/City.kt
+++ app/src/main/java/com/example/clima/City.kt
@@ -1,14 +1,14 @@
 package com.example.clima
 
-class City(name:String, degrees:Int, state:String) {
+class City(name:String, weather:ArrayList<Weather>, main:Main?) {
 
     var name:String = ""
-    var degrees:Int = 0
-    var state:String = ""
+    var weather:ArrayList<Weather>? = null
+    var main:Main? = null
 
     init {
         this.name = name
-        this.degrees = degrees
-        this.state = state
+        this.weather = weather
+        this.main = main
     }
 }
